7721ab3ef692031162eda4d17355fb072c2d7044
Shubham <shubham2002bharat@gmail.com>
2020-12-11 16:47:19 +0530

    fun-app-assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong with our server please try again later")   
+}
+
+// outputDiv.innerText= "hi i am shubham"
+
+function clickHandler(){
+  // outputDiv.innerText = "jeijeijijfij  " + txtInput.value;  
+  var inputText = txtInput.value; //taking input
+  
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json =>  {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+    })
+  .catch(errorHandler)
+};
+btnTranslate.addEventListener("click", clickHandler)
